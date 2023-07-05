78bdbdead45d2e36b07b9146e233312622405fa8
Mersio <mersiocarneiro87@gmail.com>
2023-07-05 01:25:29 -0300

    feat: my gram front end

---
+++ src/pages/Auth/Register.jsx
@@ -0,0 +1,84 @@
+import "./Auth.css";
+import { Link } from "react-router-dom";
+
+// Import Components
+import Message from "../../components/Message";
+
+// Import Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Import Redux
+import { register, reset } from "../../slices/authSlice";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+
+    dispatch(register(user));
+  };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
+  return (
+    <div id="register">
+      <h2>My Gram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ""}
+        />
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Register;
